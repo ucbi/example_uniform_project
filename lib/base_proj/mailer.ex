624546e36a3d53f01f43a0a1722c09794db3762f
Paul Statezny <paulstatezny@gmail.com>
2022-09-01 23:07:30 -0700

    Remove Ecto generated code

--- lib/base_proj/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule BaseProj.Mailer do
-  use Swoosh.Mailer, otp_app: :base_proj
-end
--- lib/base_proj/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule BaseProj.Repo do
-  use Ecto.Repo,
-    otp_app: :base_proj,
-    adapter: Ecto.Adapters.Postgres
-end
